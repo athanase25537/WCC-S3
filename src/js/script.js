4d54e54dd15f6be4fe3c9bec0367c767481c8ee6
unknown <athanase21.aps2a@gmail.com>
2023-07-16 12:03:12 +0300

    typing animation and reorganiation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
+//scroll animation
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if(entry.isIntersecting){
             entry.target.classList.add('show');
         }else{
@@ -11,3 +11,29 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// text typing animation
+const text = document.querySelector('.sec-text');
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Developpeur";
+        let textBefore = getComputedStyle(text, '::before')
+        textBefore.animation.steps = text.textContent.length;
+    }, 0);
+
+    setTimeout(() => {
+        text.textContent = "Freelancer";
+        let textBefore = getComputedStyle(text, '::before')
+        textBefore.animation.steps = text.textContent.length;
+    }, 4000);
+
+    setTimeout(() => {
+        text.textContent = "Designer";
+        let textBefore = getComputedStyle(text, '::before')
+        textBefore.animation.steps = text.textContent.length;
+    }, 8000);
+
+}
+
+textLoad();
+setInterval(textLoad, 12000);
